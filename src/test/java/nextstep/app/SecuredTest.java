3c3a1b58012eb37a239966e6d04a3cb5b1008ac7
jiyeong-dt <imjee02@gmail.com>
2025-03-05 00:53:18 +0900

    refactor: 안쓰는 임포트 정리

--- src/test/java/nextstep/app/SecuredTest.java
+++ src/test/java/nextstep/app/SecuredTest.java
@@ -2,7 +2,6 @@ package nextstep.app;
 
 import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
